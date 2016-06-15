5f832f4c9e8b342f190bc46aee7eb78313186030
Bigfish <12ebc82@sina.com>
2016-06-15 20:47:12 +0800

    20160615204700

--- js/jquery.js
+++ js/jquery.js
@@ -5,5 +5,11 @@ $(document).ready(function(){
 	$("#start").click(function(){
 		$("#initialShow").hide();
 		$("#productList").show();
-	})
-})
+	});
+	
+	$("button").hover(function(){
+		$("#product").show();
+		$("#binning").show();
+		$("#maintain").show();
+	});
+});
