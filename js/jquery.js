c5c3df46611e6cf349b4a386793c2636ff6edc48
Bigfish <12ebc82@sina.com>
2016-06-17 16:27:04 +0800

    20160617162700

--- js/jquery.js
+++ js/jquery.js
@@ -6,13 +6,17 @@ $(document).ready(function(){
 	$("#testerType").nextAll("div").hide();
 	$("button").hover(function(event){
 		var chosenTesterType=$(this).text();
-		alert(chosenTesterType);
+//		alert(chosenTesterType);
 		if (chosenTesterType=="C400") {
-			alert("this is c400 tester");
+//			alert("this is c400 tester");
+			var nickname=['Anguilla IMM', 'Anguilla Blue', 'Anguilla Black', 'Anguilla Auto', 'Aruba', 'Bahamas', 'Baja',
+				           'Hockenheim Auto', 'Hockenheim IMM'];
 		} else if(chosenTesterType=="PS800") {
-			alert("this is PS800 tester");
+			var nickname=['Tortola', 'Bono'];
+//			alert("this is PS800 tester");
 		} else {
-			alert("this is PS1600 tester");
+//			alert("this is PS1600 tester");
+			var nickname=["Senna"];
 		}
 		$("#testerType").next("div").show();
 		event.stopPropagation();
@@ -34,8 +38,8 @@ $(document).ready(function(){
 
 //arttemplat data here
 var data = {
-		nickName: ['Anguilla IMM', 'Anguilla Blue', 'Anguilla Black', 'Anguilla Auto', 'Aruba', 'Bahamas', 'Baja',
-		           'Hockenheim Auto', 'Hockenheim IMM'],
-		           hp:['C400','PS800','PS1600'],
-		           hbin:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+	nickName: ['Anguilla IMM', 'Anguilla Blue', 'Anguilla Black', 'Anguilla Auto', 'Aruba', 'Bahamas', 'Baja',
+	           'Hockenheim Auto', 'Hockenheim IMM'],
+	testerType:['C400','PS800','PS1600'],
+	hbin:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
 };
