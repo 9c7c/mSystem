ccb41fb0a09a90c964cab1bba95b419d10a04f4b
Bigfish <12ebc82@sina.com>
2016-06-17 15:43:06 +0800

    20160617154300

--- js/jquery.js
+++ js/jquery.js
@@ -2,9 +2,21 @@
  * @author Bigfish
  */
 $(document).ready(function(){
-	$("#type").nextAll("div").hide();
-	$("button").hover(function(){
-		$("#type").next("div").show();
+	
+	$("#testerType").nextAll("div").hide();
+	$("button").hover(function(event){
+		var chosenTesterType=$(this).text();
+		alert(chosenTesterType);
+		if (chosenTesterType=="C400") {
+			alert("this is c400 tester");
+		} else if(chosenTesterType=="PS800") {
+			alert("this is PS800 tester");
+		} else {
+			alert("this is PS1600 tester");
+		}
+		$("#testerType").next("div").show();
+		event.stopPropagation();
+		propStopped(event);
 	});
 	
 	$("#product a").click(function(){
@@ -16,4 +28,14 @@ $(document).ready(function(){
 		$("#binning").next("div").show();
 		return false;
 	});
+	
 });
+
+
+//arttemplat data here
+var data = {
+		nickName: ['Anguilla IMM', 'Anguilla Blue', 'Anguilla Black', 'Anguilla Auto', 'Aruba', 'Bahamas', 'Baja',
+		           'Hockenheim Auto', 'Hockenheim IMM'],
+		           hp:['C400','PS800','PS1600'],
+		           hbin:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+};
